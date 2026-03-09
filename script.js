1b2dd4be357a13090024114d712d7083e1d875d1
Camilla Oliveira <Camilla1819github.com>
2026-03-09 16:03:34 -0300

    corrigindo conflito de merge

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* TEMA (MODO ESCURO) */
 document.addEventListener("DOMContentLoaded", function () {
 
@@ -63,74 +62,4 @@ function mostrarSobre() {
     if (secao.style.display === "none") {
         secao.style.display = "block";
     } else {
-        secao.style.display = "none";
-    }
-=======
-/* TEMA (MODO ESCURO) */
-document.addEventListener("DOMContentLoaded", function () {
-
-    let botaoTema = document.getElementById("temaBtn");
-
-    if (botaoTema) {
-        botaoTema.addEventListener("click", alternartema);
-    }
-
-    let temaSalvo = localStorage.getItem("tema");
-
-    if (temaSalvo === "dark") {
-        document.body.classList.add("dark");
-    }
-});
-
-/* botão alternar tema */
-function alternartema() {
-
-    document.body.classList.toggle("dark");
-
-    if (document.body.classList.contains("dark")) {
-        localStorage.setItem("tema", "dark");
-    } else {
-        localStorage.setItem("tema", "light");
-    }
-}
-
-/* FORMULÁRIO DE CONTATO */
-let form = document.getElementById("formcontato");
-if (form) {
-    form.addEventListener("submit", function(event) {
-        event.preventDefault();
-
-        let nome = document.getElementById("nome").value;
-        let email = document.getElementById("email").value;
-        let mensagem = document.getElementById("mensagem").value;
-
-        /* verifica campos vazios */
-        if (nome === "" || email === "" || mensagem === "") {
-            alert("Por favor, preencha todos os campos!");
-            return;
-        }
-
-        /* validação de email */
-        let emailValido = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        if (!emailValido.test(email)) {
-            alert("Digite um e-mail válido! Ex: usuario@dominio.com");
-            return;
-        }
-
-        alert("Mensagem enviada com sucesso!");
-        form.reset();
-
-    });
-}
-
-/* MOSTRAR / ESCONDER SOBRE */
-function mostrarSobre() {
-    let secao = document.getElementById("sobre");
-
-    if (secao.style.display === "none") {
-        secao.style.display = "block";
-    } else {
-        secao.style.display = "none";
-    }
->>>>>>> 2969cfa72f9c488677c5499c23507c456d88c0ac
-}
+        secao.style.display = "none";}
